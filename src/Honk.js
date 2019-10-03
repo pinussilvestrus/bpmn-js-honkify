083dade05db2fe71696e5bd067f6a7603e5de981
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-10-03 11:02:24 +0200

    feat(project): add toast on every honk

--- src/Honk.js
+++ src/Honk.js
@@ -2,6 +2,8 @@ import CommandInterceptor from 'diagram-js/lib/command/CommandInterceptor';
 
 import inherits from 'inherits';
 
+import showToast from 'show-toast';
+
 export default function Honk(injector) {
 
   injector.invoke(CommandInterceptor, this);
@@ -10,16 +12,27 @@ export default function Honk(injector) {
     'https://res.cloudinary.com/jlengstorf/video/upload/q_auto/v1569957993/honk-sound.mp3',
   );
 
-  function honk(context) {
+  function _honk(context) {
 
     console.log('honk fired.', context);
 
+    // honk
     audio.play();
 
+    // show toast
+    showToast({
+      str: 'Honk 🦆🦆🦆!',
+      time: 500,
+      position: 'top'
+    });
     return false;
   }
 
-  this.execute([ 'shape.create', 'shape.move', 'shape.delete' ], honk);
+  this.execute([
+    'shape.create',
+    'shape.move',
+    'shape.delete'
+  ], _honk);
 }
 
 Honk.$inject = [
