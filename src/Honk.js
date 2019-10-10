c68e1ea675cf97cd2a4e699729760029dda08fb7
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-10-10 15:50:24 +0200

    feat(honk): honk on every granular operation
    Closes #1

--- src/Honk.js
+++ src/Honk.js
@@ -1,10 +1,15 @@
+/* global Promise */
 import CommandInterceptor from 'diagram-js/lib/command/CommandInterceptor';
 
 import inherits from 'inherits';
 
 import showToast from 'show-toast';
 
-export default function Honk(injector) {
+import {
+  reduce
+} from 'min-dash';
+
+export default function Honk(eventBus, injector) {
 
   injector.invoke(CommandInterceptor, this);
 
@@ -12,30 +17,77 @@ export default function Honk(injector) {
     'https://res.cloudinary.com/jlengstorf/video/upload/q_auto/v1569957993/honk-sound.mp3',
   );
 
-  function _honk(context) {
-
-    console.log('honk fired.', context);
-
-    // honk
-    audio.play();
-
-    // show toast
-    showToast({
-      str: 'Honk 🦆🦆🦆!',
-      time: 500,
-      position: 'top'
-    });
-    return false;
+  function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
   }
 
-  this.execute([
+  function _handler(context) {
+
+    const {
+      command
+    } = context;
+
+    let granularEvents = [];
+
+    const recordEvents = (e) => {
+      granularEvents.push({
+        ...e
+      });
+    };
+
+    const _honk = async (context) => {
+
+      console.log('honk fired.', context);
+
+      // wait for every operation to be done
+      await sleep(500);
+
+      if (!granularEvents.length) {
+        return;
+      }
+
+      console.log(`Making a honk for each ${granularEvents.length} operation 🦆.`);
+
+      // show toast
+      showToast({
+        str: 'Honk 🦆🦆🦆!',
+        time: 500,
+        position: 'top'
+      });
+
+      await reduce(granularEvents, async (promise, e) => {
+        await promise;
+
+        // honk
+        const clonedAudio = audio.cloneNode();
+        await clonedAudio.play();
+
+        await sleep(200);
+
+        console.log('🦆');
+
+      }, Promise.resolve());
+
+      granularEvents = [];
+
+      return false;
+    };
+
+    eventBus.on('commandStack.executed', recordEvents);
+
+    eventBus.on(`commandStack.${command}.postExecute`, _honk);
+
+  }
+
+  this.preExecute([
     'shape.create',
     'shape.move',
     'shape.delete'
-  ], _honk);
+  ], _handler);
 }
 
 Honk.$inject = [
+  'eventBus',
   'injector'
 ];
 
